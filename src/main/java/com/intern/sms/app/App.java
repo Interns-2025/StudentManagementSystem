ae63cb2620b726b29dacc8257e16b3835c324f92
Fazal Khan <ggl.acc007@gmail.com>
2025-04-22 14:37:31 +0530

    dummy commit

--- src/main/java/com/intern/sms/app/App.java
+++ src/main/java/com/intern/sms/app/App.java
@@ -3,6 +3,7 @@ package com.intern.sms.app;
 import com.intern.sms.controller.UserController;
 
 public class App {
+    //starting the app
     public static void main(String[] args) {
         UserController controller = new UserController();
         controller.start();
